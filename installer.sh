36689de08cd4a71d4c7b60a89b10303a646a2bee
Amirali Shahinpour <amirali.shahinpour@alation.com>
2020-07-24 09:38:20 -0700

    add authenticator and awscli into installer
    Signed-off-by: Amirali Shahinpour <amirali.shahinpour@alation.com>

--- installer.sh
+++ installer.sh
@@ -2,12 +2,23 @@
 
 set -ex
 
-#install Kubectl
+# install Kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/
 
-#install HELM
+# install HELM
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod +x get_helm.sh
 ./get_helm.sh
+
+
+# install AWS Authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.7/2020-07-08/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mv ./aws-iam-authenticator /usr/local/bin
+
+# install AWSCLI V2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
