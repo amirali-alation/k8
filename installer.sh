4e46be863a2710ce6450124c7d2649a4f79c31cc
Amirali Shahinpour <amirali.shahinpour@alation.com>
2020-07-26 09:43:16 -0700

    add kubectl autocomplete
    Signed-off-by: Amirali Shahinpour <amirali.shahinpour@alation.com>

--- installer.sh
+++ installer.sh
@@ -8,6 +8,9 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/
 
+# setup kubectl auto completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
 # install HELM
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod +x get_helm.sh
